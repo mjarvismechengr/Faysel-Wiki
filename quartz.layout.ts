fbb3fa524e72c4cea376ea5f45128fdee114819d
Max Jarvis <mjarvis.mechengr@gmail.com>
2026-01-28 13:38:11 -0800

    Update player wiki

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Components.Explorer({
+      excludeFolders: ["_Indexes"],
+    }),
   ],
   right: [
     Component.Graph(),
